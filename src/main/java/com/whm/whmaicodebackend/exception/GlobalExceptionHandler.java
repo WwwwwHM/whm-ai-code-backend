166b18d4293444eb4bba83e1d52340ea1d0408be
WwwwwHM <231060382@hdu.edu.cn>
2026-03-26 10:20:02 +0800

    add 全局跨域配置 update 全局异常处理添加@Hidden注解隐藏swagger文档，解决knife4j在OPENAPI3版本与@RestControllerAdvice冲突问题

--- src/main/java/com/whm/whmaicodebackend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/whm/whmaicodebackend/exception/GlobalExceptionHandler.java
@@ -3,13 +3,15 @@ package com.whm.whmaicodebackend.exception;
 
 import com.whm.whmaicodebackend.common.BaseResponse;
 import com.whm.whmaicodebackend.common.ResultUtils;
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
- * 全局异常处理器
+ * 全局异常处理器 （AOP切面
  */
+@Hidden // 隐藏swagger文档，解决OPENAPI3的knife4j和RestControllerAdvice的不兼容问题
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
