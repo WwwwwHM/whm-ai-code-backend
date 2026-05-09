abd5c472dba11a152901c3367f1d8cc1235e76ae
WwwwwHM <231060382@hdu.edu.cn>
2026-05-09 15:58:27 +0800

    add 用户模块后端开发：注册、登录、注销、查询、权限控制、用户管理

--- src/main/java/sql/create_table.sql
+++ src/main/java/sql/create_table.sql
@@ -3,10 +3,10 @@
 # @from <a href="https://codefather.cn">编程导航学习圈</a>
 
 -- 创建库
-create database if not exists yu_ai_code_mother;
+create database if not exists whm_ai_code_mother;
 
 -- 切换库
-use yu_ai_code_mother;
+use whm_ai_code_mother;
 
 -- 用户表
 -- 以下是建表语句
@@ -20,10 +20,15 @@ create table if not exists user
     userAvatar   varchar(1024)                          null comment '用户头像',
     userProfile  varchar(512)                           null comment '用户简介',
     userRole     varchar(256) default 'user'            not null comment '用户角色：user/admin',
+    vipExpireTime datetime     null comment '会员过期时间',
+    vipCode       varchar(128) null comment '会员兑换码',
+    vipNumber     bigint       null comment '会员编号',
+    shareCode     varchar(20)  DEFAULT NULL COMMENT '分享码',
+    inviteUser    bigint       DEFAULT NULL COMMENT '邀请用户 id',
     editTime     datetime     default CURRENT_TIMESTAMP not null comment '编辑时间',
     createTime   datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
     updateTime   datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
     isDelete     tinyint      default 0                 not null comment '是否删除',
     UNIQUE KEY uk_userAccount (userAccount),
     INDEX idx_userName (userName)
-    ) comment '用户' collate = utf8mb4_unicode_ci;
+) comment '用户' collate = utf8mb4_unicode_ci;
